0073d085c3a17f240fa8a9f63745fa874aa14975
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-11 03:11:33 +0000

    Add: Validations for the user schema.
    - The email attr must be unique.
    - The name, email, password, tel, country and the user picture
      can not be blank when the user registering.
    - Add paperclip gem validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    
+    # Validation.
+    validates_presence_of :name, :user_picture, :password, :email, :country, :tel, message: "Please fill in all the fields"
+    validates_uniqueness_of :email, message: "A user already exists with this email"
+    
+    # Paperclip gem setup
+    has_attached_file :user_picture, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :user_picture, content_type: /\Aimage\/.*\Z/
+    
 end
