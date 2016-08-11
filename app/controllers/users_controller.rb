63daa53aaf68a15731920613606481d659f6f50a
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-11 00:29:24 +0000

    New: Handle thelogin and user register
    - Add flash messages for a better user experience
    - Create the user_params method to keep the user params in one place.
    - Add the business logic to the create controller ø

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
     # GET - Show a user.
     def show
+        @user = User.find(params[:id])
     end
 
     # GET - Show template to create a user.
@@ -11,6 +12,15 @@ class UsersController < ApplicationController
 
     # POST - Create a user.
     def create
+        @user = User.new(user_params)
+        
+        if @user.save
+            flash[:success] = "Welcome to move #{user_params[:name]}"
+            redirect_to user_path(@user)
+        else
+            flash[:error] = @user.errors.full_messages[0]
+            render 'new'
+        end
     end
 
     # GET - Show template to edit a user.
@@ -30,19 +40,26 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-
     def auth_local
-        @user = User.find_by_name(params[:user][:name])
+        @user = User.find_by_email(params[:user][:email])
 
         if (@user != nil)
             if (@user.password == params[:user][:password])
                 redirect_to user_path(@user)
             else
+                flash[:error] = "Your enter a wrong password."
                 redirect_to root_path
             end
         else
+            flash[:error] = "This user doesn't exists."
             redirect_to root_path
         end
+        
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:name, :password, :email, :tel, :country, :user_picture)
     end
 
     
