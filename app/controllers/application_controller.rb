d23c61f9efd891485f5c0744d7e68de300787feb
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-23 00:50:00 -0700

    Override after_sign_in and after_sign_out devise actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+    def after_sign_in_path_for(resource_or_scope)
+        # your_path
+        user_path(current_user)
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+        # your_path
+        new_user_session_path
+    end
 end
