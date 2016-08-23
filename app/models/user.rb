f2aedd775032296202c5376ea31ae8fb1db1031b
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-23 00:56:17 -0700

    Add devise validations to the user model
    - Delete custom validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-    
-    # Validation.
-    validates_presence_of :name, :user_picture, :password, :email, :country, :tel, message: "Please fill in all the fields"
-    validates_uniqueness_of :email, message: "A user already exists with this email"
-    
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
     # Paperclip gem setup
-    has_attached_file :user_picture, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
-    validates_attachment_content_type :user_picture, content_type: /\Aimage\/.*\Z/
-    
+    has_attached_file :avatar, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
 end
