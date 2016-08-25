a0ba86a0d19a06c5780eec7055c142fd082ce076
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-24 22:39:20 -0700

    Import bootstrap and mdl javascripts files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,16 @@
 //= require jquery
 //= require jquery_ujs
 //= require materialize-sprockets
+//= require bootstrap-sprockets
+//= require material
 //= require vue/dist/vue.min
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener('turbolinks:load', function() {
+  componentHandler.upgradeDom();
+});
+
+document.addEventListener('page:change', function() {
+  componentHandler.upgradeDom();
+});
--- app/assets/javascripts/user-feed.js
+++ app/assets/javascripts/user-feed.js
@@ -1,9 +1,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require materialize-sprockets
+//= require bootstrap-sprockets
+//= material
 //= require turbolinks
 
 $(document).on('turbolinks:load', function (){
     $(".button-collapse").sideNav();
     $('.collapsible-accordion').collapsible();
+    $('.scrollspy').scrollSpy();
+    $(".alert").alert()
 });
