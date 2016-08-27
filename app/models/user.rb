237b6579c0d832943c1bfed41dc67f578c22bca2
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-27 01:36:08 -0700

    Add search class method to allows to model to search users in system

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,12 @@ class User < ApplicationRecord
     has_attached_file :avatar, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
+    def self.search(search)
+        where("first_name LIKE ?", "%#{search}%")
+        where("last_name LIKE ?", "%#{search}%")
+        where("email LIKE ?", "%#{search}%")
+    end
+
     def name
         "#{first_name} #{last_name}"
     end
