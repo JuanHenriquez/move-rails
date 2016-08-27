944c4d2d1172825aad2cbd239d5815de2576f852
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-27 01:36:49 -0700

    Create the search and send_request actions

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -15,6 +15,22 @@ class FriendsController < ApplicationController
         @requested_list = current_user.requested_friends
     end
 
+    def search
+        search_result = params[:search]
+        @results = User.search(search_result).to_a.keep_if { |user|
+            user != current_user
+        }
+
+        @results
+    end
+
+    def send_request
+        friend = User.find(params[:friend_id])
+        Friendship.request(current_user, friend)
+        flash[:success] = "Your request has been send successfully!"
+        redirect_to user_dashboard_path(current_user)
+    end
+
     def create
         friend = User.find(params[:friend_id])
         Friendship.accept(current_user, friend)
