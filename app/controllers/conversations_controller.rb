407433f08c752c2063cb17792d8d454d25ef5f74
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-29 06:47:30 -0400

    Add create and destroy actions to the ConversationsController

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,17 +1,31 @@
 class ConversationsController < ApplicationController
     before_action :authenticate_user!
-    before_action :get_mailbox
-    before_action :get_conversation, except: [:index]
+    before_action :get_mailbox, only: [:index, :show, :destroy]
+    before_action :get_conversation, except: [:index, :create]
     before_action :get_friend, only: [:show]
     layout "user-feed"
 
     def index
         # Get all the conversations for the current user.
-        @conversations = @mailbox.inbox.paginate(page: params[:page], per_page: 20)
+        @conversations = @mailbox.conversations.paginate(page: params[:page], per_page: 20)
     end
 
     def show
 
+    end
+
+    def create
+        friend = User.find(params[:friend_id])
+        conversation = current_user.send_message(friend, "Prueba", "New").conversation
+        redirect_to user_conversation_path(current_user, conversation)
+    end
+
+    def destroy
+        friend = User.find(params[:friend_id])
+        current_user.mark_as_deleted(@conversation)
+        friend.mark_as_deleted(@conversation)
+        flash[:danger] = 'Conversation was deleted!'
+        redirect_to user_conversations_path
     end
 
     private
@@ -25,7 +39,7 @@ class ConversationsController < ApplicationController
     end
 
     def get_friend
-        friend_id = @mailbox.conversations.find(2).receipts.where("receiver_id != ?", current_user.id).take.receiver_id
+        friend_id = @mailbox.conversations.find(@conversation.id).receipts.where("receiver_id != ?", current_user.id).take.receiver_id
         @friend = User.find(friend_id)
     end
 end
