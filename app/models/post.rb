3a173e1408de47a694adfe1342c39611923fd2a3
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-09-05 13:55:40 -0400

    Add dependency destroy posts models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
 
     belongs_to :user
-    has_many :likes, :class_name => "PostLike"
-    has_many :dislikes, :class_name => "PostDislike"
-    has_many :comments, :class_name => "PostComment"
+    has_many :likes, :class_name => "PostLike", dependent: :destroy
+    has_many :dislikes, :class_name => "PostDislike", dependent: :destroy
+    has_many :comments, :class_name => "PostComment", dependent: :destroy
 
 end
