b8836ccb2668f7f3474814008c07cdac195c5fd3
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-09-05 13:56:33 -0400

    Add dependency destroy in photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,9 @@
 class Photo < ApplicationRecord
 
     belongs_to :user
-    has_many :likes, :class_name => "PhotoLike"
-    has_many :dislikes, :class_name => "PhotoDislike"
-    has_many :comments, :class_name => "PhotoComment"
+    has_many :likes, :class_name => "PhotoLike", dependent: :destroy
+    has_many :dislikes, :class_name => "PhotoDislike", dependent: :destroy
+    has_many :comments, :class_name => "PhotoComment", dependent: :destroy
 
     has_attached_file :picture, styles: { large: "1000x1000>", medium: "600x600>", small: "100x100>" }
     validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
